fd897e6f97081ba1225b217433eb13afdc1cb4a4
claragnz <clara.gnz@gmail.com>
2024-06-21 22:22:28 -0300

    first commit

---
+++ app/Views/front/registro.php
@@ -0,0 +1,37 @@
+<!--formulario registro-->
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputName" class="form-label">Nombre</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputLastname" class="form-label">Apellido</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputPassword3" class="form-label">Contraseña</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Debe tener de 8 a 10 caracteres">
+  </div>
+  <div class="col-12">
+    <label for="inputPassword4" class="form-label">Repetir contraseña</label>
+    <input type="text" class="form-control" id="inputPassword4" placeholder="">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">Ciudad</label>
+    <input type="text" class="form-control" id="inputCity">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">Provincia</label>
+    <select id="inputState" class="form-select">
+      <option selected>Seleccionar</option>
+      <option>Corrientes</option>
+      <option>Chaco</option>
+      <option>Formosa</option>
+      <option>Entre Ríos</option>
+      <option>Santa Fé</option>
+    </select>
+  </div>
+    <div class="col-12">
+    <button type="submit" class="btn btn-primary">Registrarme</button>
+  </div>
+</form>
